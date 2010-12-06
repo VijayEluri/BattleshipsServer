d855415c99587015a63879881365dbb71db1f882
Doug Frazer <dcfrazer@gmail.com>
2010-12-06 15:59:19 -0500

    bunch of updates, its basically working on a really really simple scale

--- BattleshipsClient.java
+++ BattleshipsClient.java
@@ -5,40 +5,50 @@ import java.io.*;
 public class BattleshipsClient {
     private Socket s;
     private ClientThread thread;
+    private static final int SERVER_PORT = 22191;
+    private static final int NORTH = 1;
+    private static final int EAST = 2;
+    private static final int WEST = 3;
+    private static final int SOUTH = 4;
 
     public BattleshipsClient () { 
-        try { s = new Socket("scottio.us", 22); }
+        try { s = new Socket("scottio.us", 22191); }
         catch (IOException e) {e.printStackTrace();}
         thread = new ClientThread(s);
         thread.start();
-        thread.sendMessage("Test Message 1");
-        thread.sendMessage("Test Message 2");
-        thread.sendMessage("alkjdfal;kjsadf;lkjadsf;lkj");
+        thread.sendMessage("LOGIN tester1 blah");
+        thread.sendMessage("MOVE " + NORTH);
+        thread.sendMessage("MOVE " + SOUTH);
+        thread.sendMessage("garbage");
+        /*
         thread.close();
         try { thread.join(); }
-        catch (InterruptedException e) {}
+        catch (InterruptedException e) {e.printStackTrace();}
+        System.exit(0);
+        */
     }
 
     public class ClientThread extends Thread {
         private boolean running;
-        private BufferedReader br;
-        private PrintWriter pw;
+        private BufferedReader in;
+        private PrintWriter out;
         private Socket socket;
         private String msg;
         public ClientThread(Socket s) {
             running = true;
             this.socket = s;
             try {
-                br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                pw = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
             } catch (IOException e) {}
         }
         
         public void run () {
             while (running) {
                 try {
-                    while ((msg = br.readLine()) != null && running) {
+                    while ((msg = in.readLine()) != null) {
                         System.out.println("S: " + msg);
+                        msg = "";
                     }
                 } catch (IOException e) {}
             }
@@ -46,7 +56,7 @@ public class BattleshipsClient {
 
         public void sendMessage(String s) {
             System.out.println("C: " +s);
-            pw.println(s);
+            out.println(s);
         }
 
         public void close() {
--- BattleshipsServer.java
+++ BattleshipsServer.java
@@ -1,28 +1,155 @@
 import java.net.*;
 import java.io.*;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
 import java.sql.Timestamp;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class BattleshipsServer {
+public class BattleshipsServer implements BattleshipsServerInterface {
 
-    private static final int SERVER_PORT = 22;
+    private static final int SERVER_PORT = 22191;
+    private static final int WORLD_WIDTH = 100;
+    private static final int WORLD_HEIGHT = 100;
+
+    private static final int NORTH = 1;
+    private static final int EAST = 2;
+    private static final int WEST = 3;
+    private static final int SOUTH = 4;
+
     private ServerListener listener;
     private ServerSocket ssocket;
     private Socket socket;
     private Scanner in;
-    private ArrayList<ServerThread> clients;
+    private LinkedList<ServerThread> clients;
+    private KeepaliveThread keepalive;
+    private int[][] world;
+    private Lock clientLock;
 
     public BattleshipsServer () {
-        // Instantiate a single thread to listen for 
-        // all incoming connections
-        clients = new ArrayList<ServerThread>();
+        // A list of each client that has connected
+        clients = new LinkedList<ServerThread>();
+        clientLock = new ReentrantLock();
+
+        // Instantiate the console...
         System.out.print('\f');
         System.out.println("Starting the Battleships server...");
         in = new Scanner(System.in);
         new ReaderThread().start();
+
+        // Instantiate the parent thread which will be listening for new
+        // connections
         listener = new ServerListener();
         listener.start();
+
+        // Start the keepalive thread
+        keepalive = new KeepaliveThread();
+        keepalive.start();
+
+        // Instatiate the world
+        world = new int[WORLD_WIDTH][WORLD_HEIGHT];
+    }
+
+    /* Here is where we implement the Server methods */
+    public boolean move (int direction, Client c) {
+        // All moves are of one square, lets make sure
+        // that the target square is fre
+        if (c == null) return false;
+        if (c.current == null) return false;
+        Coordinate newLoc = null;
+        switch (direction) {
+            case NORTH:
+                newLoc = new Coordinate(c.current.x+1, c.current.y);
+                break;
+            case EAST:
+                newLoc = new Coordinate(c.current.x, c.current.y+1);
+                break;
+            case WEST:
+                newLoc = new Coordinate(c.current.x, c.current.y-1);
+                break;
+            case SOUTH:
+                newLoc = new Coordinate(c.current.x-1, c.current.y);
+                break;
+            default:
+                break;
+        }
+        if (newLoc == null) return false;
+        Coordinate placed = placePlayer(newLoc, c.current, c.uid, false);
+        if (placed != null) {
+            c.setLocation(placed);
+            return true;
+        }
+        return false;
+        
+    }
+
+    public boolean fire (int a, int b) {
+        return false;
+    }
+
+    public Client login (String username, String password) {
+        // Need some logic here to connect to SQL database and
+        // verify login information... later...
+        
+        // Place him in the world
+        Client tester1 = new Client (1, WORLD_WIDTH/2-1, WORLD_HEIGHT/2);
+        Client tester2 = new Client (2, WORLD_WIDTH/2+1, WORLD_HEIGHT/2);
+        Coordinate placed;
+
+        if (username.equals("tester1")) {
+            placed = placePlayer(tester1, true); 
+            if (placed == null) return null;
+            tester1.setLocation(placed);
+            return tester1;
+        } else if (username.equals("tester2")) {
+            placed = placePlayer(tester2, true); 
+            if (placed == null) return null;
+            tester2.setLocation(placed);
+            return tester2;
+        }
+
+        return null;
+    }
+
+    private Coordinate placePlayer(Client c, boolean placeNearby) {
+        return placePlayer(c.current, c.current, c.uid, placeNearby);
+    }
+
+    private Coordinate placePlayer(Coordinate current,
+                                   Coordinate previous,
+                                   int uid, boolean placeNearby) {
+        final int MAX_DISTANCE = 5;
+
+        if(world[current.x][current.y] == 0) {
+            world[previous.x][previous.y] = 0;
+            world[current.x][current.y] = uid;
+            System.out.println("<Server>: Placing player at ("+current.x+","+current.y+")");
+            return current;
+        } else if (placeNearby) {
+            int iter = 0;
+            for (int i=0;i<iter;i++) {
+                for (int j=0;j<iter;j++) {
+                    if (world[current.x+i][current.y+j] == 0) {
+                        world[previous.x][previous.y] = 0;
+                        world[current.x+i][current.y+j] = uid;
+                        System.out.println("<Server>: Placing player at ("+current.x+","+current.y+")");
+                        return new Coordinate(current.x+i, current.y+j);
+                    }
+                }
+            }
+            return null;
+        } else {
+        }
+        return null;
+    }
+
+    public class GameEngine extends Thread {
+        // The purpose of this class is to check for when time-base
+        // events in the world happen and to process them accordingly
+
+
     }
 
     public class ReaderThread extends Thread {
@@ -35,7 +162,7 @@ public class BattleshipsServer {
         public void run () {
             while (running) {
                 System.out.print("Battleships Server> ");
-                parse(BattleshipsServer.this.in.next());
+                parse(BattleshipsServer.this.in.nextLine());
             }
         }
 
@@ -44,6 +171,8 @@ public class BattleshipsServer {
                 System.out.println("Help is not available");
             } else if (s.equalsIgnoreCase("who")) {
                 System.out.println("Clients currently connected:");
+                BattleshipsServer.this.clientLock.lock();
+                try {
                 if (BattleshipsServer.this.clients.size() == 0) {
                     System.out.println("No clients connected"); 
                     return;
@@ -52,7 +181,24 @@ public class BattleshipsServer {
                     //Timestamp connected_time = new Timestamp(System.currentTimeMillis() - t.connected);
                     System.out.println(t.host + " " + t.connected);
                 }
+                } catch (Exception e) { e.printStackTrace(); 
+                } finally { 
+                    BattleshipsServer.this.clientLock.unlock();
+                }
+            } else if (s.equalsIgnoreCase("print world")) {
+                for (int i=0;i<WORLD_WIDTH;i++) {
+                    for (int j=0;j<WORLD_HEIGHT;j++) {
+                        if (world[i][j] == 0)
+                            System.out.print(".");
+                        else
+                            System.out.print(world[i][j]);
+                    }
+                    System.out.println("");
+                }
+            } else if (s.equalsIgnoreCase("exit") || s.equalsIgnoreCase("quit")) {
+                System.out.println("");
                 running = false;
+                System.exit(0);
             } else {
                 System.out.println("Command not supported");
             }
@@ -73,12 +219,20 @@ public class BattleshipsServer {
                 BattleshipsServer.this.ssocket = new ServerSocket(BattleshipsServer.this.SERVER_PORT);
 
                 while (listening) {
-                    BattleshipsServer.this.socket = BattleshipsServer.this.ssocket.accept();
-                    System.out.println("Client accepted");
+                    Socket newClient = BattleshipsServer.this.ssocket.accept();
+                    System.out.println("\nClient connected");
                     try {
-                        ServerThread t = new ServerThread(BattleshipsServer.this.socket);
-                        BattleshipsServer.this.clients.add(t);
-                    } catch (IOException e) {}
+                        ServerThread t = new ServerThread(newClient);
+                        BattleshipsServer.this.clientLock.lock();
+                        try {
+                            BattleshipsServer.this.clients.add(t);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            BattleshipsServer.this.clientLock.unlock();
+                        }
+                        t.start();
+                    } catch (IOException e) { e.printStackTrace(); }
                 }
             } catch (IOException e) {}
         }
@@ -98,43 +252,117 @@ public class BattleshipsServer {
         private boolean running;
         private String line,input;
         private PrintStream out;
+        private DataInputStream in;
+    
+        public Client client;
+        public long lastAction;
+        public boolean softTimeout;
 
         public ServerThread (Socket s) throws IOException {
             this.socket = s;
             this.host = s.getInetAddress();
+            in = new DataInputStream (socket.getInputStream());
+            out = new PrintStream(socket.getOutputStream());
+            running = true;
+            lastAction = System.currentTimeMillis();
         }
         
         public void run () {
             try {
                 // Get input from the client
-                DataInputStream in = new DataInputStream (socket.getInputStream());
-                out = new PrintStream(socket.getOutputStream());
-
                 while (running) {
-                    input = "";
-                    while((line = in.readLine()) != null && !line.equals(".")) {
-                        input=input + line;
-                        out.println("C:" + line);
+                    while((input = in.readLine()) != null) {
+                        System.out.println("C: " +input);
+                        parse(input);
+                        lastAction = System.currentTimeMillis();
+                        input = "";
                     }
-                    parse(input);
                 }
-            } catch (IOException e) { }
+            } catch (IOException e) { e.printStackTrace(); }
         }
 
         private void parse (String s) {
             // What will i do with the commands....?
-            if (s.equals("Test Message 1")) {
-                out.println("Received test message 1");
-                System.out.println("S: Received test message 1");
-            } else if (s.equals("Test Message 2")) { 
-                out.println("Received test message 2");
-                System.out.println("S: Received test message 2");
-            } else { 
-                out.println("Stop sending me garbage!");
-                System.out.println("Stop sending me garbage!");
+            String[] split = s.split(" ");
+            boolean status = false;
+            String outMsg = "ACK ";
+
+            if (split.length <= 0)
+                return;
+
+            if (split[0].equalsIgnoreCase("MOVE")) {
+                status = BattleshipsServer.this.move(Integer.parseInt(split[1]),
+                                            client);
+                if (status) {
+                    outMsg += input;
+                    sendMessage(outMsg);
+                }
+            } else if (split[0].equalsIgnoreCase("FIRE")) {
+                status = BattleshipsServer.this.fire(Integer.parseInt(split[1]),
+                                            Integer.parseInt(split[2]));
+            } else if (split[0].equalsIgnoreCase("LOGIN")) {
+                Client newClient = BattleshipsServer.this.login(split[1], split[2]);
+                if (newClient == null) {
+                    sendMessage("NAK " + s);
+                } else {
+                    this.client = newClient;
+                    outMsg += input;
+                    sendMessage(outMsg);
+                }
+            } else {
+                sendMessage("NAK " +s);
+            }
+        }
+
+        private void sendMessage(String s) {
+            System.out.println("S: " + s);
+            out.println(s);
+        }
+    }
+
+    private class KeepaliveThread extends Thread {
+        // The purpose of this thread is to find clients 
+        // who have dropped and disconnect them
+        
+        private boolean running;
+        private final int SOFT_TIMEOUT = 10000;
+        private final int HARD_TIMEOUT = 20000;
+        private final int CHECK_TIMER = 1000;
+
+        public KeepaliveThread () {
+            running = true;
+        }
+
+        public void run () {
+            while (running) {
+                long now = System.currentTimeMillis();
+                BattleshipsServer.this.clientLock.lock();
+                try {
+                    for (ServerThread t : clients) {
+                        if(now - t.lastAction > SOFT_TIMEOUT && !t.softTimeout) {
+                            System.out.println("Client " +t.host+" hit soft-timeout limit");
+                            t.softTimeout = true;
+                        }
+                        if(now - t.lastAction > HARD_TIMEOUT) {
+                            System.out.println("Client " +t.host+" hit hard-timeout limit, disconecting...");
+                            boolean retry = true;
+                            while (retry) {
+                                try {   
+                                    t.join();
+                                    retry = false;
+                                } catch (InterruptedException e) { e.printStackTrace(); }
+                            }
+                            clients.remove(t);
+                        }
+                    }
+                } catch (Exception e) { e.printStackTrace(); 
+                } finally { BattleshipsServer.this.clientLock.unlock(); }
+                try { this.sleep(CHECK_TIMER); }
+                catch (InterruptedException e) { e.printStackTrace(); }
             }
         }
     }
+
 
     public static void main(String [] args) {
         new BattleshipsServer();
