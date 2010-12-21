8a0bfd1864cbdd63addcb49b5d2fe42e936e3dbb
Doug Frazer <dcfrazer@gmail.com>
2010-12-21 17:06:07 -0500

    Seperated out the classes more properly

--- BattleshipsServerInterface.java
+++ BattleshipsServerInterface.java
@@ -1,6 +1,10 @@
 
 public interface BattleshipsServerInterface {
     
+    public static final int SERVER_PORT = 22191;
+    public static final int WORLD_WIDTH = 50;
+    public static final int WORLD_HEIGHT = 50;
+
     public boolean move (int direction, Client c);
     public boolean fire (int a, int b);
     public Client login (String username, String password);
